756d14f0bd24c30d4cb319d67e86b91dc1febac9
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-03-28 23:03:18 +0600

    file stream route added

--- server.js
+++ server.js
@@ -33,7 +33,40 @@ server.post("/upload",upload.single("video"),(req,res)=>{
         console.log(error);
     }
 });
-
+// video stream route
+server.get('/stream/:filename', (req, res) => {
+  try{
+  const path = __dirname + '/upload/videos/' + req.params.filename;
+  const stat = fs.statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1;
+    const chunksize = (end-start)+1;
+    const file = fs.createReadStream(path, {start, end});
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    };
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(path).pipe(res);
+  }
+}
+catch(err){
+  console.log(err);
+}
+});
 
 // video downloading route
 server.get("/download/:filename",(req,res)=>{
